3ac739e83caf9f70fb242b369ed56346182c4cf8
xieqian <xieqian0928@qq.com>
2019-01-03 15:16:03 +0800

    添加postcss对less的单独支持

--- webpack/rem-demo/build/webpack.base.conf.js
+++ webpack/rem-demo/build/webpack.base.conf.js
@@ -44,7 +44,19 @@ let config = {
                 use: [
                     isProd ? MiniCssExtractPlugin.loader : "style-loader",
                     "css-loader",
-                    "postcss-loader",
+                    {
+                        // 在这里单独指定了options，所以postcss不会去读取默认配置的文件[postcss.config.js]的配置
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: [
+                                require('postcss-pxtorem')({
+                                    rootValue: 72,
+                                    minPixelValue: 2,
+                                    propList: ["font", "font-size"]
+                                })
+                            ]
+                        }
+                    },
                     "less-loader"
                 ]
             }
@@ -76,6 +88,7 @@ Object.keys(moduleConfig).forEach(key => {
             title: moduleConfig[key].title || '广州天河区分局运维管理平台',
             filename: `htmls/${key}.html`,
             template: `./src/htmls/${key}.html`,
+            chunks: [key],
             hash: true
         })
     );
--- webpack/rem-demo/build/wepack.module.conf.js
+++ webpack/rem-demo/build/wepack.module.conf.js
@@ -2,6 +2,10 @@ const config = {
     index:{
         entry:'./src/js/index.js',
         title: '广州天河区分局运维管理平台'
+    },
+    user:{
+        entry:'./src/js/user.js',
+        title: '个人中心'
     }
 };
 

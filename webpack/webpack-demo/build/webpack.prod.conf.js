fbb1c32ac3d6bafb06f0fef17e8d360d087f00e0
xieqian <xieqian0928@qq.com>
2019-01-11 17:19:34 +0800

    添加publicPath备注

--- webpack/webpack-demo/build/webpack.prod.conf.js
+++ webpack/webpack-demo/build/webpack.prod.conf.js
@@ -6,7 +6,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpackConfig = merge(baseWebpackConfig, {
     mode: 'production',
     output: {
-        // 很重要
+        // 很重要，不主动添加publicPath属性时，添加到html的脚本会按照页面相对路径来引用
         publicPath: ''
     },
     module: {
